d870d2cfa5ac1f244deca171645f421400464e6d
eka0907@mail.ru <eka0907@mail.ru>
2021-11-10 03:01:45 +0300

    Task01.List

--- Task01.List/Program.cs
+++ Task01.List/Program.cs
@@ -1,4 +1,41 @@
-﻿//Что нужно сделать
+﻿var numbers = GenerateNumbers();
+DisplayNumbers(numbers);
+DeleteNumbers(numbers);
+Console.WriteLine("После удаления:");
+DisplayNumbers(numbers);
+
+static void DisplayNumbers(List<int> numbers)
+{
+    foreach (var number in numbers)
+    {
+        Console.WriteLine(number);
+    }
+}
+static List<int> GenerateNumbers()
+{
+    var numbers = new List<int>();
+    var random = new Random();
+    for (var i = 0; i < 100; i++)
+    {
+        numbers.Add(random.Next(100));
+    }
+    return numbers;
+}
+static void DeleteNumbers(List<int> numbers)
+{
+    numbers.RemoveAll(number => 25 < number && number < 50);
+}
+
+
+
+
+
+
+
+
+
+
+//Что нужно сделать
 //Создайте лист целых чисел. 
 //Заполните лист 100 случайными числами в диапазоне от 0 до 100. 
 //Выведите коллекцию чисел на экран. 
