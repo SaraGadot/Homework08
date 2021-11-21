3e6af6604631578cd98f9e768b05a6948045b6af
eka0907@mail.ru <eka0907@mail.ru>
2021-11-22 00:57:49 +0300

    Task04.NoteBook

---
+++ Task04.NoteBook/Person.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml.Linq;
+
+namespace Task04.NoteBook
+{
+    internal class Person
+    {
+        public string Name;
+        public string Street;
+        public string HouseNumber;
+        public string FlatNumber;
+        public string MobilePhone;
+        public string FlatPhone;
+
+        public XElement ToXml()
+        {
+            return new XElement("Person", new XAttribute("name", Name),
+                new XElement("Address",
+                    new XElement("Street", Street),
+                    new XElement("HouseNumber", HouseNumber),
+                    new XElement("FlatNumber", FlatNumber)
+                ),
+                new XElement("Phones",
+                    new XElement("MobilePhone", MobilePhone),
+                    new XElement("FlatPhone", FlatPhone)
+                ) 
+             );
+
+        }
+        public void Save(string fileName)
+        {
+            File.WriteAllText(fileName, ToXml().ToString());
+        }
+    }
+}
+
+//<Person name=”ФИО человека”>
+//    <Address>
+//        <Street>Название улицы</Street>
+//        <HouseNumber>Номер дома</HouseNumber>
+//        <FlatNumber>Номер квартиры</FlatNumber>
+//    </Address>
+//    <Phones>
+//        <MobilePhone>89999999999999</MobilePhone>
+//        <FlatPhone>123-45-67<FlatPhone>
+//    </Phones>
+//</Person>
---
+++ Task04.NoteBook/Program.cs
@@ -0,0 +1,78 @@
+﻿using Task04.NoteBook;
+
+var person = InputPerson();
+person.Save("person.xml");
+
+static Person InputPerson()
+{
+    Console.WriteLine("Введите ФИО:");
+    var name = Console.ReadLine();
+    Console.WriteLine("Введите название улицы:");
+    var street = Console.ReadLine();
+    Console.WriteLine("Введите номер дома:");
+    var houseNumber = Console.ReadLine();
+    Console.WriteLine("Введите номер квартиры:");
+    var flatNumber = Console.ReadLine();
+    Console.WriteLine("Введите номер мобильного телефона:");
+    var mobilePhone = Console.ReadLine();
+    Console.WriteLine("Введите номер домашнего телефона:");
+    var flatPhone = Console.ReadLine();
+    return new Person
+    {
+        Name = name,
+        Street = street,
+        HouseNumber = houseNumber,
+        FlatNumber = flatNumber,
+        MobilePhone = mobilePhone,
+        FlatPhone = flatPhone
+    };
+
+}
+
+
+
+
+
+
+
+
+
+
+
+//Задание 4.Записная книжка
+
+
+//Что нужно сделать
+//Программа спрашивает у пользователя данные о контакте:
+
+//ФИО
+//Улица
+//Номер дома
+//Номер квартиры
+//Мобильный телефон
+//Домашний телефон
+
+
+//С помощью XElement создайте xml файл, в котором есть введенная информация. 
+//XML файл должен содержать следующую структуру:
+
+//<Person name=”ФИО человека”>
+//    <Address>
+//        <Street>Название улицы</Street>
+//        <HouseNumber>Номер дома</HouseNumber>
+//        <FlatNumber>Номер квартиры</FlatNumber>
+//    </Address>
+//    <Phones>
+//        <MobilePhone>89999999999999</MobilePhone>
+//        <FlatPhone>123-45-67<FlatPhone>
+//    </Phones>
+//</Person>
+
+
+//Советы и рекомендации
+//Заполняйте XElement в ходе заполнения данных. Изучите возможности XElement в документации Microsoft.
+
+
+
+//Что оценивается
+//Программа создаёт Xml файл, содержащий все данные о контакте.
