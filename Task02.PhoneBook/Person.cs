4cd2f86c334a782bf998c7015b72a23848de702b
- <->
2021-11-29 01:13:34 +0300

    откоментирован код

--- Task02.PhoneBook/Person.cs
+++ Task02.PhoneBook/Person.cs
@@ -1,10 +1,24 @@
 ﻿namespace Task02.PhoneBook
 {
+    /// <summary>
+    /// Владелец телефона
+    /// </summary>
     internal class Person
     {
+        /// <summary>
+        ///  ФИО владельца телефона
+        /// </summary>
         public string Name;
+        /// <summary>
+        /// Номер телефона
+        /// </summary>
         public string Phone;
 
+        /// <summary>
+        /// Создать владельца телефона
+        /// </summary>
+        /// <param name="namе">ФИО владельца телефона</param>
+        /// <param name="phone">Номер телефона</param>
         public Person(string name, string phone)
         {
             Name = name;
--- Task02.PhoneBook/PhoneBook.cs
+++ Task02.PhoneBook/PhoneBook.cs
@@ -1,14 +1,26 @@
 ﻿namespace Task02.PhoneBook
 {
+    /// <summary>
+    /// Записная книжка
+    /// </summary>
     internal class PhoneBook
     {
         protected Dictionary<string, Person> personsByPhoneIndex = new Dictionary<string, Person>();
 
+        /// <summary>
+        /// Добавить владельца телефона в записную книжку
+        /// </summary>
+        /// <param name="person">Владелец телефона</param>
         public void Add(Person person)
         {
             personsByPhoneIndex[person.Phone] = person;
         }
 
+        /// <summary>
+        /// Найти владельца телефона по номеру телефона
+        /// </summary>
+        /// <param name="phone">Номер телефона для поиска</param>
+        /// <returns>Найденный владелец телефона или null, если не найдено</returns>
         public Person FindByPhone(string phone)
         {
             if (personsByPhoneIndex.TryGetValue(phone, out var person))
